b6f5d18b48d7e8930ecbf02c9495179d8a96f6ac
Alex Ticovschi <alexmticovschi@gmail.com>
2017-07-19 22:40:59 +0100

    Update centuryFromYear.js

--- centuryFromYear.js
+++ centuryFromYear.js
@@ -1,12 +1,10 @@
 // Challenge: Given a year, return the century it is in. The first century spans from the year 
 // 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
-
 // Example:
-//
-// For year = 1905, the output should be
-// centuryFromYear(year) = 20;
-// For year = 1700, the output should be
-// centuryFromYear(year) = 17.
+//      For year = 1905, the output should be
+//      centuryFromYear(year) = 20;
+//      For year = 1700, the output should be
+//      centuryFromYear(year) = 17.
 
 function centuryFromYear(year) {
     // divide the year by 100 and round the number upward to its nearest integer
