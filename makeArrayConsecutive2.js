40a40e8bb50f4adb5ce0b69f776c2618bd67462b
Alex Ticovschi <alexmticovschi@gmail.com>
2017-07-25 23:34:17 +0100

    Create makeArrayConsecutive2.js
    Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.

---
+++ makeArrayConsecutive2.js
@@ -0,0 +1,16 @@
+function makeArrayConsecutive2(statues) {
+  // 1. Find the maximum and minimum values of the arguments
+  // 2. Create a for loop, check if the number is contained in the array; if it's not, increment the count variable
+  // 3. return the result
+  var min = Math.min(...statues),
+      max = Math.max(...statues),
+      count = 0;
+
+  for (var i = min; i < max; i++ ) {
+    if (statues.indexOf(i) === -1) {
+      count++;
+    }
+  }
+  
+  return count;
+}
