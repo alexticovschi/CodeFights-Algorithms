b8ccafd7e7748e9ea0c31bdce3b44b78f9e39eb4
Alex Ticovschi <alexmticovschi@gmail.com>
2017-09-12 21:44:00 +0100

    adjacentElementsProduct.js
    Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
    Example:
       For inputArray = [3, 6, -2, -5, 7, 3], the output should be
       adjacentElementsProduct(inputArray) = 21.
    
       7 and 3 produce the largest product.

---
+++ adjacentElementsProduct.js
@@ -0,0 +1,12 @@
+// Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+// Example:
+//   For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+//   adjacentElementsProduct(inputArray) = 21.
+//   7 and 3 produce the largest product.
+
+function adjacentElementsProduct(inputArray) { 
+    var newArr = inputArray.map((num, index) => num * inputArray[index + 1]).slice(0, inputArray.length - 1);  
+    return newArr.reduce(function(a, b) {
+        return Math.max(a,b);
+    }); 
+}
