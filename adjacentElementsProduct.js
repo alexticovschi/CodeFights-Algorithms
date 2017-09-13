725658d18f5670f0d617d0c287d771e7798ed9f4
Alex Ticovschi <alexmticovschi@gmail.com>
2017-09-13 22:42:01 +0100

    Using sort method
    Using sort

--- adjacentElementsProduct.js
+++ adjacentElementsProduct.js
@@ -5,6 +5,11 @@
 //   7 and 3 produce the largest product.
 
 function adjacentElementsProduct(inputArray) { 
-    var newArr = inputArray.map((num, index) => num * inputArray[index + 1]).slice(0, inputArray.length - 1);  
-    return newArr.reduce((a, b) => Math.max(a,b));
+    //var newArr = inputArray.map((num, index) => num * inputArray[index + 1]).slice(0, inputArray.length - 1);  
+    //return newArr.reduce((a, b) => Math.max(a,b));
+    
+    // iterate over the inputArray array and multiply each number with its adjacent number (i.e. 3 * 6, 6 * -2, -2 * -5, -5 * 7, 7 * 3) 
+    // ...then sort the array from highest to lowest and then return the first number from the newArr array, i.e. the highest number(the largest product)
+    var newArr = inputArray.map((num, index) => num * inputArray[index + 1]).sort((a,b) => b > a);  
+    return newArr[0];   
 }
