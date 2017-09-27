ef51984a4b0b14cb0cdac7a42485ba032d91e2d8
Alex Ticovschi <alexmticovschi@gmail.com>
2017-09-27 22:40:00 +0100

    Update alternatingSums.js

--- alternatingSums.js
+++ alternatingSums.js
@@ -3,7 +3,6 @@ function alternatingSums(a) {
     if(a.length !== 1) {
         a.map((num, index) => (index % 2 === 0) ? team1 += a[index]: team2 += a[index]);
         return [team1, team2];
-    } else {
-        return [a[0], team2];
     }
+    return [a[0], team2];
 }
