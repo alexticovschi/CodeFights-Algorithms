d3e2c56c08968c1305f073c34860416efe08b231
Alex Ticovschi <alexmticovschi@gmail.com>
2017-10-09 21:18:45 +0100

    Update arrayChange.js

--- arrayChange.js
+++ arrayChange.js
@@ -5,9 +5,10 @@
 
 function arrayChange(inputArray) {
     let count = 0;
+    let difference = 0;
     for (let i = 0; i < inputArray.length - 1; i++) {
         if(inputArray[i] >= inputArray[i + 1]) {
-            let difference = inputArray[i] + 1 - inputArray[i + 1];
+            difference = inputArray[i] + 1 - inputArray[i + 1];
             inputArray[i + 1] = inputArray[i] + 1;
             inputArray[i]++;
             count += difference;
