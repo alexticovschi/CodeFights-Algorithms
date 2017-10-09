f504346a05550cc8d5a359563321aa8b2244da99
Alex Ticovschi <alexmticovschi@gmail.com>
2017-10-09 21:19:43 +0100

    Update checkPalindrome.js

--- checkPalindrome.js
+++ checkPalindrome.js
@@ -1,3 +1,13 @@
+// Given the string, check if it is a palindrome.
+
+// Example
+//  For inputString = "aabaa", the output should be
+//  checkPalindrome(inputString) = true;
+//  For inputString = "abac", the output should be
+//  checkPalindrome(inputString) = false;
+//  For inputString = "a", the output should be
+//  checkPalindrome(inputString) = true.
+
 function checkPalindrome(inputString) {
     // convert the string into an array of letters, reverse the order and the convert it back to a string
     var str = inputString.split('').reverse().join('');
