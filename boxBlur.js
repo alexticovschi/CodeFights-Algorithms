03f09557eb20dcd925494adf228a54021d443947
Alex Ticovschi <alexmticovschi@gmail.com>
2017-12-10 20:25:38 +0000

    Update boxBlur.js

--- boxBlur.js
+++ boxBlur.js
@@ -3,6 +3,10 @@ function boxBlur(image) {
    let colLen = image.length;    // how many elements in each column  
    let rowLen = image[0].length; // how many elements in a row
    
+   // drop a pixel of every side of the row and column or drop 2 elements from both dimensions
+   // the first 2 loops will capture the 3 by 3 grid
+   // the next 2 loops are used to iterate through the 3 by 3 grids, push the total and capture the next 3 by 3
+   // push the total into the row array and the row array into blurImg array, which will be returned
    for(let x = 0; x < colLen - 2; x++) { 
       let row = [];
       for(let y = 0; y < rowLen - 2; y++) {
