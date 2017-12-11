4d73a1daa00107835213c2486ba1094905f73f20
Alex Ticovschi <alexmticovschi@gmail.com>
2017-12-11 20:57:32 +0000

    Update adjacentElementsProduct.js

--- adjacentElementsProduct.js
+++ adjacentElementsProduct.js
@@ -4,12 +4,20 @@
 //   adjacentElementsProduct(inputArray) = 21.
 //   7 and 3 produce the largest product.
 
-function adjacentElementsProduct(inputArray) { 
-    //var newArr = inputArray.map((num, index) => num * inputArray[index + 1]).slice(0, inputArray.length - 1);  
-    //return newArr.reduce((a, b) => Math.max(a,b));
+function adjacentElementsProduct(inputArray) {
     
-    // iterate over the inputArray array and multiply each number with its adjacent number (i.e. 3 * 6, 6 * -2, -2 * -5, -5 * 7, 7 * 3) 
-    // ...then sort the array from highest to lowest and then return the first number from the newArr array, i.e. the highest number(the largest product)
-    var newArr = inputArray.map((num, index) => num * inputArray[index + 1]).sort((a,b) => b > a);  
-    return newArr[0];   
+    //SECOND SOLUTION
+    // var newArr = inputArray.map((num, index) => num * inputArray[index + 1]).slice(0, inputArray.length - 1);  
+    // return newArr.reduce(function(a, b) {
+    //    return Math.max(a,b);
+    // }); 
+    
+    //THIRD SOLUTION
+    // iterate over the inputArray array and multiply each number with its adjacent number (i.e. 3 * 6, 6 * -2, -2 * -5, -5 * 7, 7 * 3)
+    // then sort the array from highest to lowest, and then return the first number from the newArr array, i.e. the highest number(the largest product)
+    // 
+    // let newArr = inputArray.map((num, index) => num * inputArray[index + 1]).sort((a,b) => b > a);  
+    // return newArr[0]; 
+    
+    return inputArray.map((num, index) => num * inputArray[index + 1]).sort((a,b) => b > a)[0]; 
 }
