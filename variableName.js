fbe8e5c3f86ee4090a197931d49000129d8abdb0
Alex Ticovschi <alexmticovschi@gmail.com>
2017-12-29 21:03:33 +0000

    Update variableName.js

--- variableName.js
+++ variableName.js
@@ -1,3 +1,13 @@
+// Correct variable names consist only of Latin letters, digits and underscores and they can't start with a digit.
+// Check if the given string is a correct variable name.
+// Example
+//  For name = "var_1__Int", the output should be
+//      variableName(name) = true;
+//  For name = "qq-q", the output should be
+//      variableName(name) = false;
+//  For name = "2w2", the output should be
+//      variableName(name) = false.
+
 function variableName(name) {
     if(name.match(/\[/g) || name.match(/\]/g) || name.match(/\-/g) || name.match(/\ /g)) {
         return false;
