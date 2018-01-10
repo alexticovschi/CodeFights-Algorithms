83b8bb2b321a19397810c6c0619f8c6b7e404d66
Alex Ticovschi <alexmticovschi@gmail.com>
2018-01-10 21:10:12 +0000

    Update alphabeticShift.js

--- alphabeticShift.js
+++ alphabeticShift.js
@@ -3,16 +3,25 @@
 //  For inputString = "crazy", the output should be
 //  alphabeticShift(inputString) = "dsbaz".
 
+// ### First Solution
+// function alphabeticShift(inputString) {
+//     ascii_code = [];
+    
+//     for(let i = 0; i < inputString.length; i++) {
+//         ascii_code.push(inputString[i].charCodeAt(0));
+//     }
+    
+//     for(let i = 0; i < ascii_code.length; i++) {
+//         ascii_code[i] == 122 ? ascii_code[i] = 97 : ascii_code[i]++;
+//     }
+    
+//     return ascii_code.map(code => String.fromCharCode(code)).join('');
+// }
+
 function alphabeticShift(inputString) {
     ascii_code = [];
+    for(let i = 0; i < inputString.length; i++) ascii_code.push(inputString[i].charCodeAt(0));
     
-    for(let i = 0; i < inputString.length; i++) {
-        ascii_code.push(inputString[i].charCodeAt(0));
-    }
-    
-    for(let i = 0; i < ascii_code.length; i++) {
-        ascii_code[i] == 122 ? ascii_code[i] = 97 : ascii_code[i]++;
-    }
-    
-    return ascii_code.map(code => String.fromCharCode(code)).join('');
+    return ascii_code.map(code => code == 122 ? code = 97 : code + 1 )
+                     .map(code => String.fromCharCode(code)).join('');
 }
